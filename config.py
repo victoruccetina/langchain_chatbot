c3d0eba5d834437e661e898d9477a7170d6d3d95
Victor Uc Cetina <victor.uc@stxnext.pl>
2024-11-07 14:10:46 -0600

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+def set_environment():
+    os.environ["OPENAI_API_KEY"] = "k-proj-VKIhBdhroBLKt0NUJMKYKs93n2XcUcgul8tT5j_UWTpL4GQAqaCvaC8ve9-iFtvNu4jpfr5G55T3BlbkFJUh4mAZXqHga0yNWl-0FHcMlSTT9mwh0i4BE6pVzgtOiUbefK6xnFiMdkmcyF2lFFeZXOXNGikA"  # <- this has to be your api key!
+    os.environ["GOOGLE_API_KEY"] = "IzaSyAzizk3YJWPYSPUN5tY--PslMevCHJhCx8"
+    # I'm omitting all other keys
